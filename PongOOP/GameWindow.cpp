6c37c24a3ee205654d41d4a79f090723e80c79b0
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2024-10-11 14:21:42 +0100

    Moved entire game loop and rending into the function renderGame in the GameWindow class. This function has the entire loop. In the Main class the GameSession Object simply calls the function to render the game.
    Inside the GameWindow class, I've cleaned up the RenderGame by creating specific functions for the goal Scored Check and the Collision Check. The logic for these 2 functionalities are in each of its functions improving readability of the overall renderGame function.

--- PongOOP/GameWindow.cpp
+++ PongOOP/GameWindow.cpp
@@ -9,17 +9,11 @@ GameWindow::GameWindow()
     GameWindow::windowWidth = 1920;
     GameWindow::windowHeight = 1080;
 
-    ////RENDER WINDOW
-    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "TicTacToe");
-    //sf::RectangleShape background(sf::Vector2f(windowWidth, windowHeight));
-    //background.setFillColor(sf::Color::Black);
-    //window.setVerticalSyncEnabled(true);
+    // VARIABLES
+    GameWindow::bColided1 = false;
+    GameWindow::bColided2 = false;
+    GameWindow::colisionDelay = 0;
 
-    //sf::Event event;
-    //std::default_random_engine generator;
-    //std::uniform_real_distribution<double> distribution(-1, 1);
-
-    //sf::Clock clock;
 }
 
 GameWindow::~GameWindow()
@@ -67,3 +61,93 @@ void GameWindow::drawScoreAndCenterLine(sf::RenderWindow& window) {
     window.draw(player2Score);
 
 }
+
+void GameWindow::goalScoredCheck(Ball& ball) {
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution(-1, 1);
+
+    // Goal Scored Checks
+    if (ball.ballCoords[2] == -100) {
+        score[1] += 1;
+
+        float random_angle = distribution(generator) * (5 * std::numbers::pi) / 12;
+        random_angle >= 0 ? random_angle = std::numbers::pi - random_angle : ball.ballCoords[2] = -std::numbers::pi - random_angle;
+
+        ball.ballCoords = { 960, 540, random_angle };
+    }
+
+    if (ball.ballCoords[2] == 2000) {
+        score[0] += 1;
+
+        float random_angle = distribution(generator) * (5 * std::numbers::pi) / 12;
+
+        ball.ballCoords = { 960, 540, random_angle };
+    }
+}
+
+void GameWindow::collisionChecks(Ball& ball, PlayerBar& player1Bar, PlayerBar player2Bar)
+{
+    bColided1 = ball.ballBarCollision(player1Bar.playerCoords);
+    bColided2 = ball.ballBarCollision(player2Bar.playerCoords);
+    if (bColided1 && colisionDelay > 1) {
+        colisionDelay = 0;
+        ball.bounceDirectionCalculation(player1Bar.playerCoords);
+    }
+
+    if (bColided2 && colisionDelay > 1) {
+        colisionDelay = 0;
+        ball.bounceDirectionCalculation(player2Bar.playerCoords);
+        ball.ballCoords[2] >= 0 ? ball.ballCoords[2] = std::numbers::pi - ball.ballCoords[2] : ball.ballCoords[2] = -std::numbers::pi - ball.ballCoords[2];
+    }
+}
+
+void GameWindow::renderGame(PlayerBar player1Bar, PlayerBar player2Bar, Ball ball) 
+{
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "TicTacToe");
+    sf::Event event;
+    sf::Clock clock;
+    sf::RectangleShape background(sf::Vector2f(windowWidth, windowHeight));
+    background.setFillColor(sf::Color::Black);
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen()) {
+
+        //while (window.pollEvent(event)) {
+        window.pollEvent(event);
+
+        switch (event.type) {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { player1Bar.updateBarPosition(window); }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { player2Bar.updateBarPosition(window); };
+
+        colisionDelay += clock.getElapsedTime().asSeconds();
+        sf::Time elapsed = clock.restart();
+
+        goalScoredCheck(ball);
+
+        collisionChecks(ball, player1Bar, player2Bar);
+
+        ball.ballMovement(elapsed);
+
+        // Rendering
+        window.clear();
+        window.draw(background);
+
+        player1Bar.drawPlayerBar(window);
+        player2Bar.drawPlayerBar(window);
+
+        drawScoreAndCenterLine(window);
+
+        ball.drawBall(window, bColided1, bColided2);
+
+        window.display();
+    }
+
+}
+
+
--- PongOOP/GameWindow.h
+++ PongOOP/GameWindow.h
@@ -4,10 +4,12 @@
 #include "SFML/Graphics.hpp"
 #include <numbers>
 #include <random>
+#include "PlayerBar.h"
+#include "Ball.h"
 
 using namespace std;
 
-class GameWindow{
+class GameWindow {
 public:
     int score[2];
 
@@ -15,9 +17,19 @@ public:
     int windowWidth;
     int windowHeight;
 
+    bool bColided1;
+    bool bColided2;
+    float colisionDelay;
+
 public:
     GameWindow();
     ~GameWindow();
 
-    void drawScoreAndCenterLine();
+    void renderGame(PlayerBar player1Bar, PlayerBar player2Bar, Ball ball);
+
+    void drawScoreAndCenterLine(sf::RenderWindow& window);
+
+    void goalScoredCheck(Ball& ball);
+
+    void collisionChecks(Ball& ball, PlayerBar& player1Bar, PlayerBar player2Bar);
 };
